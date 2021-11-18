09312c6fa051a77c54a57a693126f323acfa9036
Zuxelus <zzz256@ukr.net>
2021-11-18 11:00:44 +0200

    Next update

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 var list = {};
 
-loadJSON(function (response) {
-    var data = JSON.parse(response);
-    //var data = JSON.parse('[{"asteroid":"terra","seed":"SNDST-A-16001-0","list":[{"id":"hq","x":128,"y":189},{"id":"aetn","x":84,"y":111},{"id":"aetn","x":129,"y":327},{"id":"aetn","x":220,"y":169},{"id":"steam","x":188,"y":156},{"id":"methane","x":145,"y":128},{"id":"steam","x":72,"y":91},{"id":"methane","x":119,"y":127},{"id":"salt_water","x":135,"y":66},{"id":"oil_reservoir","x":159,"y":43},{"id":"oil_reservoir","x":144,"y":40},{"id":"oil_reservoir","x":209,"y":52},{"id":"vacillator","x":160,"y":149},{"id":"vacillator","x":105,"y":230},{"id":"vacillator","x":203,"y":122},{"id":"vacillator","x":223,"y":197},{"id":"vacillator","x":45,"y":40},{"id":"hot_co2","x":108,"y":47},{"id":"hot_hydrogen","x":191,"y":213},{"id":"hot_water","x":209,"y":307},{"id":"oil_drip","x":229,"y":59},{"id":"slush_water","x":127,"y":259},{"id":"hot_steam","x":182,"y":198},{"id":"methane","x":222,"y":79},{"id":"hot_co2","x":156,"y":89},{"id":"slush_water","x":22,"y":163},{"id":"hot_water","x":124,"y":237},{"id":"hot_po2","x":154,"y":74},{"id":"big_volcano","x":201,"y":40}]}]');
+//loadJSON(function (response) {
+//    var data = JSON.parse(response);
+    var data = JSON.parse('[{"asteroid":"terra","seed":"SNDST-A-16001-0","list":[{"id":"hq","x":128,"y":189},{"id":"aetn","x":84,"y":111},{"id":"aetn","x":129,"y":327},{"id":"aetn","x":220,"y":169},{"id":"steam","x":188,"y":156},{"id":"methane","x":145,"y":128},{"id":"steam","x":72,"y":91},{"id":"methane","x":119,"y":127},{"id":"salt_water","x":135,"y":66},{"id":"oil_reservoir","x":159,"y":43},{"id":"oil_reservoir","x":144,"y":40},{"id":"oil_reservoir","x":209,"y":52},{"id":"vacillator","x":160,"y":149},{"id":"vacillator","x":105,"y":230},{"id":"vacillator","x":203,"y":122},{"id":"vacillator","x":223,"y":197},{"id":"vacillator","x":45,"y":40},{"id":"hot_co2","x":108,"y":47},{"id":"hot_hydrogen","x":191,"y":213},{"id":"hot_water","x":209,"y":307},{"id":"oil_drip","x":229,"y":59},{"id":"slush_water","x":127,"y":259},{"id":"hot_steam","x":182,"y":198},{"id":"methane","x":222,"y":79},{"id":"hot_co2","x":156,"y":89},{"id":"slush_water","x":22,"y":163},{"id":"hot_water","x":124,"y":237},{"id":"hot_po2","x":154,"y":74},{"id":"big_volcano","x":201,"y":40}]}]');
     var t = document.getElementById('list');
     for (var i = 0; i < data.length; i++) {
         var row = t.insertRow();
@@ -27,7 +27,7 @@ loadJSON(function (response) {
         cell.innerHTML = '<div class="ant-card" onclick="openMap(this.parentElement.parentElement.id)"><div class="ant-card-body"><div class="ant-row-flex">' + left + right + '</div></div></div>';
         list[data[i].seed] = counts;
     }
-});
+//});
 
 function addValue(key, value) {
     return '<div class="entity-chip"><div class="entity-chip-img-container"><img class="entity-chip-img" src="/ONISeeds/images/' + key + '.png"></div>' +
@@ -43,7 +43,9 @@ addFilter();
 
 function addFilter() {
     f++;
-    document.getElementById('filters').innerHTML += '<div><select id="f' + f + '"></select><input type="number" id="fn' + f + '" style="width: 100px;"></input></div>';
+    var div = document.createElement('div');
+    div.innerHTML = '<select id="f' + f + '"></select><select id="fo' + f + '"><option value="1">at least</option><option value="2">at most</option><option value="3">exactly</option></select><input type="number" id="fn' + f + '" style="width: 100px;"></input>';
+    document.getElementById('filters').appendChild(div);
     addOptions('f' + f);
 }
 
@@ -59,13 +61,23 @@ function addOptions(id) {
 function filterSet() {
     var i = 0;
     for (const [key, value] of Object.entries(list)) {
-        var q = 0;
         var isTrue = i < 21;
-        for (i = 1; i <= f; i++)
-            if (document.getElementById('fn' + i).value > 0) {
-                q = value[document.getElementById('f' + i).value];
-                isTrue = isTrue && (q && q >= document.getElementById('fn' + i).value);
-            }
+        for (i = 1; i <= f; i++) {
+            var val = document.getElementById('fn' + i).value;
+            if (val == '')
+                val = 0;
+            var opt = document.getElementById('fo' + i).value;
+            var q = value[document.getElementById('f' + i).value];
+            if (!q)
+                q = 0;
+
+            if (opt == 1)
+                isTrue = isTrue && q >= val;
+            if (opt == 2)
+                isTrue = isTrue && q <= val;
+            if (opt == 3)
+                isTrue = isTrue && q == val;
+        }
         if (isTrue) {
             document.getElementById(key).style.display = 'block';
             i++;
